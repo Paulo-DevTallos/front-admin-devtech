8af19550dd0ef935a58dff6fed035d52f2ae6344
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-06-10 16:01:43 -0300

    feat: create password revelling

---
+++ src/components/atoms/RevellingPassword/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <span class="view-eyes" @click="$emit('toggle')">
+    <Icon icon="carbon:view" id="view-icon" v-if="isPassword"	/>
+		<Icon icon="carbon:view-off" id="view-icon" v-else/>
+  </span>
+</template>
+
+<script>
+import { Icon } from "@iconify/vue";
+
+export default {
+  name: 'RevellingPassword',
+  components: { Icon },
+  emits: ["toggle"],
+  props: {
+    isPassword: {
+      type: Boolean,
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../../assets/sass/mixins/alignment.scss';
+.view-eyes {
+  @include displayAlignment(flex, center, center);
+}
+</style>
