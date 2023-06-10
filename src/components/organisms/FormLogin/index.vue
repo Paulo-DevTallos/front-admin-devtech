6704e79ea1205bb27489822b28cb762971686706
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-06-10 17:37:02 -0300

    feat: create form organism

---
+++ src/components/organisms/FormLogin/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <form @submit.prevent="$emit('teste', employee)">
+    <BaseInput
+      v-model="employee.employee_email"
+      :hideRevellingPassword="false"
+      placeholder="E-mail"
+    />
+    <BaseInput
+      v-model="employee.password"
+      :hideRevellingPassword="true"
+      placeholder="Senha"
+    />
+    <button type="submit">Login</button>
+  </form>
+</template>
+
+<script>
+import BaseInput from '../../../components/molecules/BaseInput/index.vue';
+
+export default {
+  name: 'FormLogin',
+  components: { BaseInput },
+  data() {
+    return {
+      employee: {
+        employee_email: "teste",
+        password: "123456"
+      }
+    }
+  }
+}
+</script>
