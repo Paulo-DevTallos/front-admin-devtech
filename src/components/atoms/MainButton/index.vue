2ad678445402c55c10bc3fed287453a36c8e4fda
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-06-11 11:04:21 -0300

    feat: create button component

---
+++ src/components/atoms/MainButton/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <button v-bind="$attrs" class="def-btn">
+    {{ btn_title }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'MainButton',
+  props: {
+    btn_title: {
+      type: String
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
