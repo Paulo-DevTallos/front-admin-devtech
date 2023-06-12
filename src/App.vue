c5ffea9e0e5a9466d0ce4d28b60d2838b4322b9d
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-06-12 19:33:24 -0300

    feat: add socket connection

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
-<template> 
+<template>
   <RouterView />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import { socket } from './service/socket/index';
 
 export default defineComponent({
   name: 'App',
+  methods: {
+    connect() {
+      socket.connect()
+    }
+  }
 })
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
