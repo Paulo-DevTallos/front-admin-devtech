37bcba622ef75dcd777d31dec5451915c8ff7ff7
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-06-19 19:44:10 -0300

    feat: criando componente de tipografia

---
+++ src/components/Typography.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="typography-spacing">
+    <h2>
+      {{ message_page }}
+    </h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Typography',
+  props: {
+    message_page: { type: String }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.typography-spacing {
+  padding: 20px 10px;
+  display: flex;
+  align-items: center;
+
+  h2 {
+    font-weight: 400;
+    font-size: 1.8rem;
+    margin-right: 10px;
+  }
+}
+</style>
