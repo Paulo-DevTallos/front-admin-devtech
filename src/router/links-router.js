fb4eb0de15d9ecc0ff248ddf714867f4857c67f5
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-06-19 21:11:21 -0300

    feat: criando menu de rotas navegaveis

---
+++ src/router/links-router.js
@@ -0,0 +1,32 @@
+export default [
+	{
+		id: 0,
+		router: "/app/dashboard",
+		icon: "chart-combo-stacked",
+		description: "Dashboard",
+	},
+	{
+		id: 1,
+		router: "/app/employees",
+		icon: "folder-add",
+		description: "Funcionários",
+	},
+	{
+    id: 2,
+		router: "/app/customers",
+		icon: "user",
+		description: "Clientes",
+	},
+  {
+    id: 3,
+    router: "/app/store",
+    icon: "report",
+    description: "Estoque",
+  },
+  {
+		id: 4,
+		router: "/app/orders",
+		icon: "ibm-watson-orders",
+		description: "Pedidos",
+	},
+];
